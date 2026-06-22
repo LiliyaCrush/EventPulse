e112f4426f47ae4aace93ad7cf9e6cfff329c99e
Liliya Ypiskunova <lilyapiskunova@gmail.com>
2026-06-22 12:20:52 -0500

    Create EventService

---
+++ EventPulse/src/main/java/com/liliya/eventpulse/service/EventService.java
@@ -0,0 +1,37 @@
+package com.liliya.eventpulse.service;
+
+import com.liliya.eventpulse.entity.Event;
+import com.liliya.eventpulse.repository.EventRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    public Event saveEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    public void deleteEvent(Long id) {
+        eventRepository.deleteById(id);
+    }
+
+    public Event getEventById(Long id) {
+        return eventRepository.findById(id).orElse(null);
+    }
+
+    public List<Event> searchEvents(String title) {
+        return eventRepository.findByTitleContainingIgnoreCase(title);
+    }
+}
