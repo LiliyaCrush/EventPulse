49de52d2f7d974edd409b25aa33c7b6c962aedc6
Liliya Ypiskunova <lilyapiskunova@gmail.com>
2026-06-22 12:40:09 -0500

    Create EventController

---
+++ EventPulse/src/main/java/com/liliya/eventpulse/controller/EventController.java
@@ -0,0 +1,36 @@
+package com.liliya.eventpulse.controller;
+
+import com.liliya.eventpulse.entity.Event;
+import com.liliya.eventpulse.service.EventService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping("/")
+    public String viewHomePage(Model model) {
+        model.addAttribute("events", eventService.getAllEvents());
+        return "index";
+    }
+
+    @GetMapping("/showNewEventForm")
+    public String showNewEventForm(Model model) {
+        Event event = new Event();
+        model.addAttribute("event", event);
+        return "new_event";
+    }
+
+    @PostMapping("/saveEvent")
+    public String saveEvent(@ModelAttribute("event") Event event) {
+        eventService.saveEvent(event);
+        return "redirect:/";
+    }
+}
